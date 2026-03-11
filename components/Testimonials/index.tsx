d5f715f07db0118372cf65fe9d61a38fa9cb4b2c
KumarS8494 <kumartankhush786@gmail.com>
2026-03-11 23:48:51 +0530

    Fix Designation undefined variable error

--- components/Testimonials/index.tsx
+++ components/Testimonials/index.tsx
@@ -6,7 +6,7 @@ const testimonialData: Testimonial[] = [
   {
     id: 1,
     name: "Mr. Suman Kumar",
-    designation: "Research Scientist, Amity University, Noida.",
+    designation: "Project Research Scientist, Amity University, Noida.",
     content:"Working on the core algorithms for MustardSense has been a journey in precision and impact. We've successfully integrated dual-stream networks to ensure that even the smallest anomalies in crop health are detected in real-time. It is rewarding to see complex AI transition from a lab environment into a tool that offers tangible value to farmers. My focus remains on refining our models to ensure the highest diagnostic accuracy in the field.",
     image: "/images/testimonials/sk_profile.jpeg",
     star: 5,
@@ -14,7 +14,7 @@ const testimonialData: Testimonial[] = [
   {
     id: 2,
     name: "Dr. Rakesh Chandra Joshi",
-    designation: "Associate Professor, Amity University, Noida.",
+    designation: "Assistant Professor, Amity University, Noida.",
     content:
       "Our objective with MustardSense was to bridge the gap between advanced deep learning and practical agricultural application. By incorporating Explainable AI (XAI), we aren’t just giving farmers a result; we are giving them a reason they can trust. This project represents a significant step forward in making high-tech analytics accessible and intuitive for the agricultural community. I am proud of the balance we’ve struck between technical sophistication and user-centric design.",
     image: "/images/testimonials/Rakesh_Chandra_Joshi.jpg",

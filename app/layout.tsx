283c8eadc58603714538c758f435128474f98c16
kumarsuman <73792049+KumarS8494@users.noreply.github.com>
2026-05-14 16:20:32 +0530

    back to previous version seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
-import DefaultSeo from 'next-seo';
+import { DefaultSeo } from 'next-seo';
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 import { Providers } from "./providers";
